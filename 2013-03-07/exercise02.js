a06a4ce2cd956ef7f7d782ef61384443e5dfec6a
dav1razzino <dav.razzino@hotmail.it>
2013-03-08 11:46:50 +0100

    miglioramento

--- 2013-03-07/exercise02.js
+++ 2013-03-07/exercise02.js
@@ -1,8 +1,8 @@
 function fibonacci (i)	{
-	if ((i === 0) || (i === 1) || (i === 2)) 
-		res = 1;
-	else
-		res = fibonacci(i-1) + fibonacci(i-2);			
-  
-  return res;
+if (!(i in fibonacci))
+    fibonacci[i] = fibonacci(i-1) + fibonacci(i-2);
+  return fibonacci[i];
 }
+
+fibonacci[0] = 0;
+fibonacci[1] = 1;
